37a7866d49959df41632008dfe4945a54450312a
Gerry <gerryliu36@gmail.com>
2018-07-16 21:43:44 -0500

    youtube vid+pause

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
 import React, { Component } from 'react';
 import './App.css';
+import YouTube from '@u-wave/react-youtube';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      video: "Fm5iP0S1z9w",
+      paused: false
+    };
+  }
+
+  handlePause = (event) => {
+    this.setState({
+      paused: event.target.checked,
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        Test
+        <input
+          type="checkbox"
+          id="paused"
+          checked={this.state.paused}
+          onChange={this.handlePause}
+        />
+        <div className="col s9 center-align">
+          <YouTube
+            video={"Fm5iP0S1z9w"}
+            width={640}
+            height={480}
+            autoplay={true}
+            controls={false}
+            paused={this.state.paused}
+          />
+        </div>
       </div>
+      
     );
   }
 }
