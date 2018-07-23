82e8ecb81da9141aaf4e163e2c498eb5a3273d1a
Gerry <gerryliu36@gmail.com>
2018-07-23 00:53:43 -0500

    express backend added

--- server.js
+++ server.js
@@ -1,15 +1,38 @@
+// const express = require('express');
+// const app = express();
+// var router = express.Router();
+
+// var port = process.env.PORT || 8080
+// var path = require('path');
+
+// app.use(express.json());
+
+// // app.use(express.static(path.resolve(__dirname, 'build')));
+
+// app.get('/', (req, res) => {
+//     //res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+//     console.log('hello');
+// })
+
+// app.get('/api/test', (req, res) => {
+//     res.send([
+//         {
+//             getURL: "https://www.youtube.com/watch?v=i0p1bmr0EmE"
+//         }
+//     ]);
+// });
+
+// app.listen(port, () => {
+//     console.log("app running");
+// })
+
 const express = require('express');
+
 const app = express();
+const port = process.env.PORT || 8080;
 
-var port = process.env.PORT || 8080
-var path = require('path');
+app.get('/api/hello', (req, res) => {
+  res.send({ express: 'https://www.youtube.com/watch?v=PQnFbMxfbMw' });
+});
 
-app.use(express.static(path.resolve(__dirname, 'build')));
-
-app.get("*", function(req, res) {
-    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-})
-
-app.listen(port, function() {
-    console.log("app running");
-})
+app.listen(port, () => console.log(`Listening on port ${port}`));
--- src/Home.js
+++ src/Home.js
@@ -15,6 +15,21 @@ class Home extends Component {
 			chatName: ""
 		};
 	}
+	
+	componentDidMount() {
+		this.callApi()
+		  .then(res => this.setState({ url: res.express }))
+		  .catch(err => console.log(err));
+	  }
+	
+	  callApi = async () => {
+		const response = await fetch('/api/hello');
+		const body = await response.json();
+	
+		if (response.status !== 200) throw Error(body.message);
+	
+		return body;
+	  };
 
 	handlePauseCheckbox = (event) => {this.setState({ playing: event.target.checked });}
 
